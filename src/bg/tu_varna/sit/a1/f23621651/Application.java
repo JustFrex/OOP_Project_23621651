fdc6ae43a6efda04d6b8da33cfc8d7086353f98c
ndimi <ndimitrov04@abv.bg>
2025-03-08 11:45:48 +0200

    init commit

---
+++ src/bg/tu_varna/sit/a1/f23621651/Application.java
@@ -0,0 +1,9 @@
+package bg.tu_varna.sit.a1.f23621651;
+
+public class Application
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/bg/tu_varna/sit/a1/f23621651/FileHandler.java
@@ -0,0 +1,6 @@
+package bg.tu_varna.sit.a1.f23621651;
+
+public class FileHandler
+{
+
+}
